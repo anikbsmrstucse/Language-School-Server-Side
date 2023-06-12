7b776d6cfd52dcf1081d9653c117d3caa6788f7d
Anik Islam <aanikbsmrstucse@gmail.com>
2023-06-12 12:46:42 +0600

    added user info api

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middle ware
 app.use(cors());
 app.use(express.json());
+
+
+// mongodb username and password
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
+// mongodb connection
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.6euurqe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classesCollection = client.db('langSCHOOL').collection('courses');
+    const userCollection = client.db('langSCHOOL').collection('users');
+    
+    // users api
+    app.post('/users',async(req,res)=>{
+      const user = req.body;
+      const query = {email:user?.email};
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:'user is exist'})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+    
+    // all classes
+    app.get('/classes',async(req,res)=>{
+        const result = await classesCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // server running connection
 app.get('/',(req,res)=>{
